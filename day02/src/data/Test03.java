6289ce22547e38465cb0da8faf82edf0d2bf44ef
rlaalsrn285 <rlaalsrn285@naver.com>
2023-12-05 14:33:28 +0900

    2시까지한거

---
+++ day02/src/data/Test03.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		int adultPriceList = 12000;
+		int youthPriceList = 8500;
+		
+		int adultCount = 2;
+		int youthCount = 2;
+		
+		int a = adultPriceList*adultCount; //성인가격합
+		int b = youthPriceList*youthCount; // 청소년가격합
+		int total = (adultPriceList*adultCount) + (youthPriceList*youthCount);
+		
+		System.out.println("청소년은"+a+"원");
+		System.out.println("성인은"+b+"원");
+		System.out.println("합"+total+"원 입니다");
+	
+	
+	
+
+	}
+
+}
