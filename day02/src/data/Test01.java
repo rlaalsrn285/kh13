2e919def3a16f7393de69e96e21e7603e5175fa2
rlaalsrn285 <rlaalsrn285@naver.com>
2023-12-05 13:23:28 +0900

    데이터와정보 의미

---
+++ day02/src/data/Test01.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Test01 {
+    public static void main(String[] args) {
+    	//데이터(data)
+    	//데이터란 측정된값(수치)이다
+    	// 예)당산동 온도,습도,미세먼지농도 
+    	//정보(information)
+    	//정보란 데이터를 가공해서만든결과물
+    	// 예 당산동 11월평균기온 (11월에 기온데이터를 수집해서만든것)
+    	
+    	System.out.println("123-123"); //글자출력
+    	System.out.println(123-123); // 식의결과출력
+    	
+    	//7000원자리 식권10장 계산
+    	System.out.println(7000*10);
+    	
+    
+    	
+	}
+
+}
