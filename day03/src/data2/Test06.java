9dd0cffcaeb6076f9ba85d4bbb02dfda0424d695
rlaalsrn285 <rlaalsrn285@naver.com>
2023-12-06 18:24:35 +0900

    asdfasdf

---
+++ day03/src/data2/Test06.java
@@ -0,0 +1,19 @@
+package data2;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		
+		int num = 275;
+		
+		int a = num/10*10 + 1;  //첫페
+		int b = (num/10 + 1)*10; //막페
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+
+	}
+
+}
