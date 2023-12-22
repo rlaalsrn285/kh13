9076aef7a2594fac7372819dbf303c7e30e9dd87
rlaalsrn285 <rlaalsrn285@naver.com>
2023-12-22 14:36:23 +0900

    ;ㅣㅏㅓ

---
+++ day15/src/oop/multi/comedian.java
@@ -0,0 +1,5 @@
+package oop.multi;
+
+public interface comedian {
+
+}
---
+++ day15/src/oop/multi/person.java
@@ -0,0 +1,12 @@
+package oop.multi;
+
+public class person implements singer{
+
+	@Override
+	public void sing() {
+		System.out.println("노래함");
+	}
+	
+	
+
+}
