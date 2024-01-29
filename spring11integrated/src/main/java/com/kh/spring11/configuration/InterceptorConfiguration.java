849d695f3a81b13a1d55784a407d8865ae9c4332
user1 <user1@DESKTOP-SKD3K0Q>
2024-01-29 18:18:24 +0900

    asdf

---
+++ spring11integrated/src/main/java/com/kh/spring11/configuration/InterceptorConfiguration.java
@@ -0,0 +1,38 @@
+package com.kh.spring11.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import interceptor.AdminInterceptor;
+import interceptor.MemberInterceptor;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+
+	@Autowired
+	private MemberInterceptor memberInterceptor;
+	
+	@Autowired
+	private AdminInterceptor adminInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(memberInterceptor)
+		.addPathPatterns("/member/**")
+		.excludePathPatterns(
+				//"/member/join", "/member/joinFinish",
+				"/member/join*",
+				"/member/login", "/member/exitFinish"
+		);
+
+		//관리자 인터셉터 등록
+		//registry.addInterceptor(adminInterceptor)
+		//			.addPathPatterns("/admin/**");
+
+		
+	}
+	
+	
+}
